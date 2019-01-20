fcb32eaed7b89f3bafcc218aacf8b0312814c170
tc19980705 <41533677+tc19980705@users.noreply.github.com>
2019-01-20 15:01:29 +0800

    Update BaiduNewSpider.py

--- BaiduNewSpider.py
+++ BaiduNewSpider.py
@@ -1,98 +1,85 @@
 # -*- coding: utf-8 -*-
+
 import requests
 from lxml import etree
-from BaiduNewSpider import *
 import time
+import datetime
 
-class CountBaiduUrls(object):
-    def __init__(self, baiduNewSpiderObject):
-        self.headers = headers
-        self.baiduNewSpider = baiduNewSpiderObject
+BaseUrl = "http://news.baidu.com/ns?word=%s&bt=%s&et=%s&rn=100&ie=utf-8&ct=2"
 
-    def getData(self,crawl_url):
-        try:
-            data = requests.get(crawl_url, headers=self.baiduNewSpider.header)
-            time.sleep(0.1)
-            html = data.text
-            return html
-        except Exception as t:
-            with open('log.txt', 'a') as f:
-                f.write(str(t) + '\n')
+MaxSearchSize = 100
 
-    def hasNextPage(self, html):
+class BaiduNewSpider(object):
+
+    def __init__(self, keyWord, header, startTime, stopTime, timeStep, dataBase=None):
+        super().__init__()
+
+        self.keyWord = keyWord
+        self.header = header
+        self.database = dataBase
+        self.startTime = datetime.datetime.strptime(startTime, "%Y-%m-%d %H:%M:%S")
+        self.stopTime = datetime.datetime.strptime(stopTime, "%Y-%m-%d %H:%M:%S")
+        self.timeStep = datetime.timedelta(hours=timeStep)
+        self.urls = []
+
+    def searchSize(html):
         try:
             tree = etree.HTML(html)
-            next_page = tree.xpath('//div[@id="wrapper"]/p[@id="page"]/a[contains(string(), "下一页")]/@href')
-            if next_page != []:
-                next_page_url = "http://news.baidu.com" + next_page[0]
+            tempResult = tree.xpath('//span[@class="nums"]/text()')[0]
+
+            if tempResult[6].isdigit():
+                temp = tempResult[6: -1]
+                temp = int(temp.replace(",", ""))
             else:
-                next_page_url = None
-            return next_page_url
-        except Exception as t:
-            with open('log.txt', 'a') as f:
-                f.write(str(t) + '\n')
-            print("======================\n\n出现异常\n\n=====================")
+                temp = tempResult[7: -1]
+                temp = int(temp.replace(",", ""))
+            return temp
+        except Exception as e:
+            raise e
+            
+    def makeUrls(keyWord, startTime, stopTime, timeStep):
+        searchUrls = []
+        # url = BaseUrl % (keyWord, str(startTime), str(stopTime))
+        #
+        # print(url)
+        # html = requests.get(url, headers=headers).text
+        # time.sleep(1)
+        # if BaiduNewSpider.searchSize(html) > MaxSearchSize:
+        #     medainTime = (stopTime + startTime) / 2
+        #
+        #     searchUrls += BaiduNewSpider.makeUrls(keyWord, startTime, medainTime, headers)
+        #     searchUrls += BaiduNewSpider.makeUrls(keyWord, medainTime, stopTime, headers)
+        # else:
+        #     searchUrls.append(url)
+        i = 0
+        endTime = 0
+        while endTime < time.mktime(stopTime.timetuple()):
+            beginTime = time.mktime((startTime + i * timeStep).timetuple())
+            endTime = time.mktime((startTime + (i + 1) * timeStep).timetuple())
+            url = BaseUrl % (keyWord, str(beginTime), str(endTime))
+            i += 1
+            searchUrls.append(url)
 
-
-    def getResult(self, html):
-        result = []
-        try:
-            tree = etree.HTML(html)
-            temp_result = tree.xpath('//div[@id="content_left"]/div[3]//div[@class="result"]')
-            for div in temp_result:
-                temp = {
-                    'url' : None,
-                }
-                link = div.xpath('h3/a/@href')[0]
-                temp['url'] = link
-                result.append(temp)
-        except Exception as t:
-                with open('log.txt', 'a') as f:
-                    f.write(str(t) + '\n')
-                print("======================出现异常=====================")
-        return result
-
-    def run(self):
-        totalurlnum = 0
-        for crawl_url in self.baiduNewSpider.urls:
-            print("[正在计算URL条数]%s" % crawl_url)
-            i = 1
-            totalurlnums = 0
-            while crawl_url != None:
-                html = self.getData(crawl_url)
-                try:
-                    if i == 1:
-                        print("该页显示共有数据:\t%d" % BaiduNewSpider.searchSize(html))
-                    result = self.getResult(html)
-                    #print(result)
-                    print("第%d页:\t%d" % (i,len(result)))
-                    i += 1
-                    totalurlnums  += len(result)
-                    totalurlnum += len(result)
-                    crawl_url = self.hasNextPage(html)
-                except Exception as t:
-                    with open('log.txt', 'a') as f:
-                        f.write(str(t) + '\n')
-            print("新闻实际条数为%d条" % totalurlnums)
-            print("+" * 100)
-        print("新闻总数为%d条" % totalurlnum)
+        return searchUrls
 
 
 if __name__ == "__main__":
     headers = {
         'Host': 'news.baidu.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36\
+         (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
     }
     keyWord = u"权健"
 
-    search1 = BaiduNewSpider(
+    search = BaiduNewSpider(
         keyWord,
         headers,
-        "2018-01-08 00:00:00",
-        "2019-01-08 00:00:00",
-        1
+        "2018-11-06 00:00:00",
+        "2018-12-08 00:00:00",
+        24
     )
-    search1.urls = BaiduNewSpider.makeUrls(search1.keyWord, search1.startTime, search1.stopTime, search1.timeStep)
+    
+    search.urls = BaiduNewSpider.makeUrls(search.keyWord, search.startTime, search.stopTime, search.timeStep)
+    print(search.urls)
+    print(len(search.urls))
 
-    search2 = CountBaiduUrls(search1)
-    search2.run()
